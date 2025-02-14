c18133a54cc1b8895559c18c905a22decf639467
craigkibera <craigituro@gmail.com>
2025-02-14 15:34:21 +0300

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,8 @@
 
 def return_evens(num_list):
     pass
+    return [eve for eve in num_list if eve % 2 == 0]
 
 def make_exclamation(sentence_list):
     pass
+    return [sentence + "!" for sentence in sentence_list]
